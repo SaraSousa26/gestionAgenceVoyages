6b63f3e23bc666fef736c175def21a30712706fa
SaraSousa26 <sarafilipa26@gmail.com>
2022-08-22 16:24:20 +0200

    first commit

---
+++ src/main/java/com/inti/gestionAgenceVoyages/GestionAgenceVoyagesApplication.java
@@ -0,0 +1,13 @@
+package com.inti.gestionAgenceVoyages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAgenceVoyagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAgenceVoyagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inti/gestionAgenceVoyages/GestionAgenceVoyagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti.gestionAgenceVoyages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAgenceVoyagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
