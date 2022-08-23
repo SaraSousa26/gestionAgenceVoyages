2523bde1977a283a047090a14104200d17372291
ClaudeVeillas <claude.veillas@gmail.com>
2022-08-23 10:03:10 +0200

    modif Claude

--- src/main/java/com/inti/entities/Avis.java
+++ src/main/java/com/inti/entities/Avis.java
@@ -5,15 +5,16 @@ import javax.persistence.Entity;
 @Entity
 public class Avis {
 	private Long idAvis;
-<<<<<<< HEAD
+//<<<<<<< HEAD
 	private String commentairesss;
-=======
+//=======
 	private String commentaire;
 	private int nbrEtoiles;
->>>>>>> 919a4f841d4c6f73795131e58b2b5866627ae9c9
+//>>>>>>> 919a4f841d4c6f73795131e58b2b5866627ae9c9
 	
 	public Avis() {
-	
+		Sting cqfd;
+		return cqfd;
 	}
 
 	public Avis(Long idAvis, String commentaire) {
